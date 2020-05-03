78138878c8a3012738eb13da9ea37a79bc113d10
Saollin <gregory.janosz@gmail.com>
2020-05-03 02:29:47 +0200

    Correct init of FilterStructure

--- Filters.py
+++ Filters.py
@@ -10,10 +10,8 @@ class Filters(Enum):
 
 
 class FilterStructure:
-    forms = dict()
-    filter_kind = None
-
     def __init__(self, filter):
+        self.forms = dict()
         label_of_first = Labels.get_label_from_flectional_label(filter[1])
         if label_of_first == Labels.PRZYSLOWEK:
             self.forms[Przyslowek.Positive_Form] = (filter[0], filter[1])
@@ -27,15 +25,17 @@ class FilterStructure:
             self.filter_kind = Filters.AdjectionComparison
         elif label_of_first == Labels.CZASOWNIK:
             self.forms[Czasownik.Infinitive] = (filter[0], filter[1])
-            self.forms[Czasownik.Present_Adverbial_Participle] = (filter[2], filter[3])
-            self.forms[Czasownik.Active_Adjectival_Participle] = (filter[4], filter[5])
-            self.forms[Czasownik.Passive_Adjectival_Participle] = (filter[6], filter[7])
-            self.forms[Czasownik.Perfect_Adverbial_Participle] = (filter[8], filter[9])
+            participles = [Czasownik.Present_Adverbial_Participle, Czasownik.Active_Adjectival_Participle,
+                           Czasownik.Passive_Adjectival_Participle, Czasownik.Perfect_Adverbial_Participle]
+            for index, participle in enumerate(participles):
+                if filter[2 * index + 2].isalpha():  # nie jest puste
+                    self.forms[participle] = (filter[2 * index + 2], filter[2 * index + 3])
             if len(filter) == 10:
                 self.filter_kind = Filters.Participles
             elif len(filter) == 12:
                 self.filter_kind = Filters.ParticiplesAndGerundive
-                self.forms[Czasownik.Gerundive] = (filter[10], filter[11])
+                if filter[10].isalpha():  # nie jest puste
+                    self.forms[Czasownik.Gerundive] = (filter[10], filter[11])
 
     def __repr__(self):
         return str(self.forms) + str(self.filter_kind)
